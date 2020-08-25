48f72af2619cb27300292ecbf63b4707db0ce49e
97arkham <97arkham>
2020-08-25 17:37:48 +0700

    ahahahaha

---
+++ novice/02 - 01/kasus/kasus1,2.js
@@ -0,0 +1,56 @@
+function Alpha(){}
+
+Alpha.prototype.isAlpha = function(letter){
+    return /^[a-zA-Z]$/.test(letter);
+};
+
+function Vowel(){}
+Vowel.prototype = new Alpha();
+Vowel.prototype.constructor = Vowel;
+
+Vowel.prototype.isVowel = function(letter){
+    return /^[aeiuoyAEIOUY]$/.test(letter);
+};
+
+
+function Consonant(){}
+Consonant.prototype = new Alpha();
+Consonant.prototype.constructor = Consonant;
+
+Consonant.prototype.isConsonant = function(letter){
+    return /^[^aeiouyAEIOUY]$/.test(letter);
+}
+
+
+function processData(input) {
+    var a = new Alpha();
+    console.log(`'${input[0]}' is ${a.isAlpha(input[0])? ``: `not `}a letter.`);
+    
+    var b = new Vowel();
+    console.log(`'${input[1]}' is ${b.isAlpha(input[1])? `a letter ${b.isVowel(input[1])? `and`: `but not`} a vowel.`: "not a letter."} `);
+    
+    var c = new Consonant();
+    console.log(`'${input[2]}' is ${c.isAlpha(input[2])? `a letter ${c.isConsonant(input[2])? `and`: `but not`} a consonant.`: "not a letter."} `);
+    
+} 
+var a = new Alpha();
+// console.log(a.isAlpha("A"));
+if(true){
+    console.log("A is "+ a.isAlpha("A")+" ? : not a letter" );
+}else{
+    console.log("gagal");
+}
+var b = new Vowel();
+// console.log(b.isVowel("e"));
+if(true){
+    console.log("A is "+ a.isAlpha("A")+" ? : not a letter" );
+}else{
+    console.log("gagal");
+}
+var c = new Consonant();
+// console.log(c.isConsonant("r"));
+if(true){
+    console.log("r");
+}else{
+    console.log("gagal");
+}
