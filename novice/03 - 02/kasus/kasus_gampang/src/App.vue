e487923dffccca09c1b749b8936c64a7819cdda1
97arkham <97arkham>
2020-09-03 13:32:34 +0700

    ggg

---
+++ novice/03 - 02/kasus/kasus_gampang/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App" :sukoco="name" :cobas="arr"/> <!-- belajar array -->
+   
+    <gridsatu />
+     <modal />
+    <!-- <tree2 /> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import modal from './components/modal.vue'
+// import tree2 from './components/tree2.vue'
+import gridsatu from './components/gridsatu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    modal,
+    gridsatu,
+     
+    // tree2
+  },
+  data() {
+    return {
+     name: "irun",
+     arr:["a","b","c"]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ novice/03 - 02/kasus/kasus_gampang/src/components/modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>haoo</h1>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+      <!-- use the modal component, pass in the prop -->
+      <modal2 v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">custom header</h3>
+      </modal2>
+      
+  </div>
+</template>
+
+<script>
+import modal2 from './modal2'
+export default {
+  
+  data: ()=>{
+    return{
+      showModal: false
+    }
+  },
+  components:{
+        modal2
+    }
+}
+</script>
+
